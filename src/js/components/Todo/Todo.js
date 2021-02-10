ec130eb08c576290caaf280c731c8622879b1a25
Graudeep <grisha.perevalov@gmail.com>
2021-02-11 01:26:16 +0500

    Оживил кнопку свитчера.

--- src/js/components/Todo/Todo.js
+++ src/js/components/Todo/Todo.js
@@ -5,12 +5,16 @@ export default () => {
 
   const [todoSwitcher, setTodoSwitcher] = useState(false);
 
+  function switchTodo() {
+    setTodoSwitcher(true);
+  }
+
   return (
     <>
       <header className = "task__header"></header>
       <main className = "task__main">
-        <section className = "task__wrapper">
-          
+        <section onClick = {switchTodo} className = "task__wrapper">
+          <span className = "task__text">Write a new task</span>
         </section>
       </main>
       <footer className = "task__footer"></footer>
