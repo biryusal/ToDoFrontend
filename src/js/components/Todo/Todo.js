7bc7100dd7d8c232e2055a1f5e8e57245494de61
Graudeep <grisha.perevalov@gmail.com>
2021-02-11 01:29:09 +0500

    Кастомизировал поведение обёртки при нажатии.

--- src/js/components/Todo/Todo.js
+++ src/js/components/Todo/Todo.js
@@ -13,7 +13,7 @@ export default () => {
     <>
       <header className = "task__header"></header>
       <main className = "task__main">
-        <section onClick = {switchTodo} className = "task__wrapper">
+        <section onClick = {switchTodo} className = {todoSwitcher ? "task__wrapper task__wrapper_backgroundColor_white" : "task__wrapper"}>
           <span className = "task__text">Write a new task</span>
         </section>
       </main>
