4dc2f37deb14717ede4959a856acdf19ef4732a6
Graudeep <grisha.perevalov@gmail.com>
2021-02-11 01:17:41 +0500

    Сделал разметку страницу и добавил хук для Todo.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./sass/style.scss";
 import { Provider} from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 import App from "./App";
 
 ReactDOM.render(
--- src/js/components/Todo/Todo.js
+++ src/js/components/Todo/Todo.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Todo.scss";
 
 export default () => {
+
+  const [todoSwitcher, setTodoSwitcher] = useState(false);
+
   return (
-    <h1>Начало: 22:54 по МСК.</h1>
+    <>
+      <header className = "task__header"></header>
+      <main className = "task__main">
+        <section className = "task__wrapper">
+          
+        </section>
+      </main>
+      <footer className = "task__footer"></footer>
+    </>
   )
 }
